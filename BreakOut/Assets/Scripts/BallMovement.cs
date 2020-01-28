703b24348ed97dfea959c8d917132b0ae36807bb
Jocelyn Wagner <jocelynmwagner@gmail.com>
2020-01-28 18:34:51 -0500

    updated name to 2020

---
+++ BreakOut/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallMovement : MonoBehaviour
+{
+    public bool lost = true; 
+    public KeyCode space;
+    public GameObject child;
+    public GameObject newParent;
+    public int score = 0 ;
+    public Text countText;
+    public Text livesText;
+    public int lives = 5;
+    public GameObject gameOverScreen;
+
+    void launchBall()
+    {
+        lost = false; 
+        transform.SetParent(null); 
+        
+        float forceX = Random.Range(-1f, 1f);
+        float forceY = 1f;
+        
+        //transform.position = new Vector3(0f, 1.2f, 0f);
+
+        Vector3 force = new Vector3(forceX, forceY, 0);
+        float magnitude = 600;
+
+        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        GetComponent<Rigidbody>().AddForce(force * magnitude);
+    }
+        
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(space) && lost)
+        {
+            launchBall();
+        }
+
+        if(lives <= 0)
+        {
+            gameOverScreen.SetActive(true);
+            transform.position = new Vector3(0f, -3.15f, 0f);
+            newParent.transform.position = new Vector3(0f, -3.75f, 0f);
+        }
+    }
+
+   
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "BottomWall")
+        {
+            lost = true;
+            transform.position = new Vector3(0f, -3.15f, 0f);
+            newParent.transform.position = new Vector3(0f, -3.75f, 0f);
+
+            child.transform.parent = newParent.transform;
+
+            GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0f, 0f, 0f);
+
+            lives = lives - 1;
+            SetLivesText();
+        }
+
+        if (collision.gameObject.CompareTag("Brick"))
+        {
+            Destroy(collision.gameObject);
+            score = score + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + score.ToString();
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+    }
+
+}
---
+++ BreakOut/Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+    public float paddleSpeed = 1f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(rightKey) && transform.position.x < 6.50f)
+            gameObject.transform.position = new Vector3(transform.position.x + paddleSpeed, transform.position.y, 0);
+
+        if (Input.GetKey(leftKey) && transform.position.x > -6.50f)
+            gameObject.transform.position = new Vector3(transform.position.x - paddleSpeed, transform.position.y, 0);
+    }
+}
